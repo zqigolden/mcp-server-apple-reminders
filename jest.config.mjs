0803272c322a1ab2ac5a6b7f51b9322ed3272c82
Frad LEE <fradser@gmail.com>
2025-09-01 15:42:12 +0800

    chore: add biome linting and update typescript config

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,36 +3,32 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      tsconfig: {
-        module: 'CommonJS',
-        target: 'ES2020',
-        moduleResolution: 'Node',
-        allowSyntheticDefaultImports: true,
-        esModuleInterop: true,
-        isolatedModules: true,
-        types: ['jest', 'node']
-      }
-    }],
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          module: 'CommonJS',
+          target: 'ES2020',
+          moduleResolution: 'Node',
+          allowSyntheticDefaultImports: true,
+          esModuleInterop: true,
+          isolatedModules: true,
+          types: ['jest', 'node'],
+        },
+      },
+    ],
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  testMatch: [
-    '<rootDir>/src/**/*.test.ts',
-    '<rootDir>/src/**/*.spec.ts'
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/dist/',
-    '<rootDir>/dist/'
-  ],
+  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/src/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '<rootDir>/dist/'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.test.ts',
     '!src/**/*.spec.ts',
     '!src/**/*.d.ts',
-    '!src/**/__mocks__/**'
+    '!src/**/__mocks__/**',
   ],
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 };
